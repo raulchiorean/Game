060992b4ed2ed9b7c27cc699c08d2d3b60c5b782
weakness100 <raul_chiorean97@yahoo.ro>
2019-05-21 21:26:52 +0300

    meniu

---
+++ Game/Assets/Sprites/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void MENU_ACTION_GoToPage(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    public void ExitButton()
+    {
+        Debug.Log("Exit game");
+        Application.Quit();
+    }
+}
